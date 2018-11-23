6cb4bd3b696d09b65facfd52a081360430b01fe8
Willy <willyexe97@gmail.com>
2018-11-23 14:04:27 -0300

    Signed-off-by: Willy <willyexe97@gmail.com>

---
+++ Esp8266_ThingSpeak/Esp8266_ThingSpeak.ino
@@ -0,0 +1,56 @@
+#include <ESP8266WiFi.h>
+
+String apiKey = "L1YUQLGTOATM2IEX";     //  Enter your Write API key from ThingSpeak
+
+const char *ssid =  "Visitantes";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "Visitante3574";
+const char* server = "api.thingspeak.com";
+
+WiFiClient client;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi conectado");
+
+}
+
+void loop()
+{
+  if (client.connect(server, 80))  //   "184.106.153.149" or api.thingspeak.com
+  {
+
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(WiFi.RSSI());
+
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);;
+  }
+  client.stop();
+
+  Serial.println("Enviando...");
+
+  delay(10000);
+}
